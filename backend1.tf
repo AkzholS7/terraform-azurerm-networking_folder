c8970dbcbb99d51589aa7f6202a885d7991b58ab
Akzhol <suborovakzhol7@gmail.com>
2023-02-08 14:20:50 +0600

    Second commit

--- backend1.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "akzhol-rg"
-#     storage_account_name = "akzholex70e2b"
-#     container_name       = "akzhol"
-#     key                  = "terraform.tfstate"
-#   }
-# } 
