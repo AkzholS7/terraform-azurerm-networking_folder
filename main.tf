51b0401cf05cf5c89365daa561ce83e0b988a68c
Akzhol <suborovakzhol7@gmail.com>
2023-02-08 14:28:46 +0600

    third commit

--- main.tf
+++ main.tf
@@ -64,3 +64,53 @@ resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   network_security_group_id = azurerm_network_security_group.app_nsg.id
 
 }
+
+resource "azurerm_lb" "example" {
+  name                = "lb-${var.name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  loadbalancer_id = azurerm_lb.example.id
+  name            = "BackEndAddressPool"
+}
+
+
+resource "azurerm_lb_nat_pool" "lbnatpool" {
+  resource_group_name            = var.resource_group_name
+  name                           = "ssh"
+  loadbalancer_id                = azurerm_lb.example.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_probe" "example" {
+  loadbalancer_id = azurerm_lb.example.id
+  name            = "http-probe"
+  protocol        = "Http"
+  request_path    = "/" #sdFkdsgfkdjgoitjgiotgjiotrjgiotrjgiojtriogjrdtgijrtighjthijothjoi\shorjhpstrhjstihjsrio
+  port            = var.azurerm_lb_probe_port
+}
+
+
+
+resource "azurerm_lb_rule" "lb-rule" {
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  backend_port                   = 80
+  frontend_port                  = 80
+  probe_id                       = azurerm_lb_probe.example.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
